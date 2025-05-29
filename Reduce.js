c0ec310a1cd5aedf83a32d3badfe20e5c444d197
solo-coder14 <bassdominator50@gmail.com>
2025-05-29 12:46:29 +0600

    new file

--- Reduce.js
+++ Reduce.js
@@ -1,16 +1,23 @@
-const people = [
-    { name: 'Alice', age: 21 },
-    { name: 'Bob', age: 21 },
-    { name: 'Charlie', age: 25 }
-  ];
-  
-  const grouped = people.reduce((acc, person) => {
-    const age = person.age;
-    if (!acc[age]) acc[age] = [];
-    acc[age].push(person);
-    return acc;
-  }, {});
-  
-  console.log(grouped);
+// const people = [
+//     { name: 'Alice', age: 21 },
+//     { name: 'Bob', age: 21 },
+//     { name: 'Charlie', age: 25 }
+//   ];
 
-  
+//   const grouped = people.reduce((acc, person) => {
+//     const age = person.age;
+//     if (!acc[age]) acc[age] = [];
+//     acc[age].push(person);
+//     return acc;
+//   }, {});
+
+//   console.log(grouped);
+
+
+const arr = [1, 2, 3, 4, 5];
+const reducer = (acc, currIndex) => {
+  return acc = acc + currIndex;
+}
+
+let result = arr.reduce(reducer, 5);
+console.log(result);
