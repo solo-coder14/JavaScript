e85d4a99e9cd7323a5c3487c6e0e61f491fcf5f8
Mohammad Woafi <woafisun@yahoo.com>
2025-10-21 18:40:22 +0600

    up 8

--- test5.js
+++ test5.js
@@ -1,21 +1,8 @@
-function isAnagram(s, t) {
-  if (s.length !== t.length) return false
-
-  let charMap = new Map();
-
-  for (let char of s) {
-    charMap.set(char, (charMap.get(char) || 0) + 1)
-  }
-
-  for (let char of t) {
-    if(!charMap.has(char)) return false
-    charMap.set(char, charMap.get(char) - 1)
-    if (charMap.get(char) === 0) charMap.delete(char)
-  }
-
-  return charMap.size == 0;
+let s = ["h","e","l","l","o"];
+for (let i = 0, j = s.length - 1; i < j; i++, j--){
+    let temp = s[i];
+    s[i] = s[j]
+    s[j] = temp
 }
 
-let s = "anagram";
-let t = "nagaram";
-console.log(isAnagram(s, t)); // Output: true
+console.log(s)
