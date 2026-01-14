f031885c93fd30b7ecb2d100a863eb75510206e4
Mohammad Woafi <woafisun@yahoo.com>
2026-01-14 13:34:02 +0600

    up10

---
+++ Value after Decimal.js
@@ -0,0 +1,2 @@
+let a = 324.3424234
+console.log(a.toFixed(2))
--- variable Data Type.js
+++ variable Data Type.js
@@ -1,26 +1,43 @@
 console.log("This is the output of console.log")
 
-// var x = 3;
-// var y = 2;
-// var z = "woafi";
-// console.log(x ** y +" "+ z)
-// console.log(typeof x, typeof z)
+var x = 3;
+var y = 2;
+var z = "woafi";
+console.log(x ** y +" "+ z)
+console.log(typeof x, typeof z)
 
-// const a = 89;
-// console.log(a)
+const a = 89;
+console.log(a)
 
-// let q = 10;
-// {
-//     let q = 15;
-//     console.log(q)
-// }
-// console.log(q)
+let q = 10;
+{
+    let q = 15;
+    console.log(q)
+}
+console.log(q)
 
-// const o = {
-//     name: "Alice",
-//     true: { required: 2, isTrue: true }
-// };
-// console.log(o.true.required)
-let a = 324.3424234
-console.log(a.toFixed(2));
+const o = {
+    name: "Alice",
+    true: { required: 2, isTrue: true }
+};
+console.log(o.true.required)
 
+
+// Null 
+let value = null;
+
+// Correct ways:
+value === null             // true
+value == null              // true (but also true for undefined)
+Object.prototype.toString.call(value)  // "[object Null]"
+
+// Array 
+let arr = [];
+
+// Correct ways:
+Array.isArray(arr)        // true (modern, recommended)
+arr instanceof Array      // true (works in most cases)
+Object.prototype.toString.call(arr)  // "[object Array]"
+
+// Don't use:
+typeof arr === "array"    // false - typeof never returns "array"
